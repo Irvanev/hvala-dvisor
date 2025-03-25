8ebd02543ca4c55dd452d4ed91af49a26355294e
Ксюша Лягашкина <xuxa@192.168.1.9>
2025-03-25 21:03:05 +0300

    update, something wrong

--- __previewjs__/Wrapper.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import "../src/index.css";
-
-export function Wrapper({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
-}
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import RestaurantPage from './pages/RestaurantPage/RestaurantPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import './App.css';
 
@@ -11,15 +9,9 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        {/* Публичные маршруты */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/restaurant/:id" element={<RestaurantPage />} />
-
 
         <Route path="/profile" element={<ProfilePage />} />
       
-        {/* Маршрут по умолчанию */}
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
